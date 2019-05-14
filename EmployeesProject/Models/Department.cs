2663b52a260e68147ec28ce2ae5b557c6e2cf6f5
vshtefaniuk <viktor.shtefaniuk@onseo.biz>
2019-05-14 19:31:19 +0300

    removed unnecessary code

--- EmployeesProject/Models/Department.cs
+++ EmployeesProject/Models/Department.cs
@@ -8,6 +8,5 @@ namespace EmployeesProject.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        //public List<Employee> Employees { get; set; }
     }
 }
