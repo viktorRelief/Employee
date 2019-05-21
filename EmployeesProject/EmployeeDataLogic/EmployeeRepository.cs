0a56f3b8343b94a9c4f3b2305b4cb43c4e329b28
vshtefaniuk <viktor.shtefaniuk@onseo.biz>
2019-05-21 17:45:44 +0300

    removed unnecessary reference

--- EmployeesProject/EmployeeDataLogic/EmployeeRepository.cs
+++ EmployeesProject/EmployeeDataLogic/EmployeeRepository.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
